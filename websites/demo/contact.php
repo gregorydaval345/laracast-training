1018123ec594faaf22a3eba7548e2e90df978e92
gregorydaval345 <davalgregory69@gmail.com>
2022-12-07 16:24:16 +0000

    Superglobals and Current Page Styling

--- websites/demo/contact.php
+++ websites/demo/contact.php
@@ -2,4 +2,5 @@
 
 $heading = "Contact Us";
 
+require('functions.php');
 require "views/contact.view.php";
--- websites/demo/our-mission.php
+++ websites/demo/our-mission.php
@@ -2,4 +2,5 @@
 
 $heading = "Our Mission";
 
+require('functions.php');
 require "views/our-mission.view.php";
