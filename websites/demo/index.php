b1cda3254208dbb81f7a71723c880c51800d1be4
gregorydaval345 <davalgregory69@gmail.com>
2023-01-09 15:26:17 +0000

    SQL injection vulnerabilities explained

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -39,9 +39,17 @@ require 'Database.php';
 // connect to our MySQL database : pdo(php data objects)
 
 $config = require('config.php');
-
 $db = new Database($config);
-$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+
+$id = $_GET['id'];
+$query = "select * from posts where id = ?";
+
+// dd($query);
+
+// $posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query($query, [$id])->fetch();
+
+dd($posts);
 
 
 
